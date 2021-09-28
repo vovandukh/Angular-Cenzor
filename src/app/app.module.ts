5a419af74ea551c7224319195daedb1cd0223dea
vovandukh <vovan302010@gmail.com>
2021-09-28 19:25:11 +0300

    add new file

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CenzorComponent } from './cenzor/cenzor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CenzorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
